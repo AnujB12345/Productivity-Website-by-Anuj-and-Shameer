4d052758e34238148f9356617c0ba90347d0f1d6
Shameer <shameerarif41@gmail.com>
2026-08-01 06:03:14 +0100

    The notes section has been upgraded, however resizing still needs to be configured

--- apps/backend/notes/models.py
+++ apps/backend/notes/models.py
@@ -4,6 +4,8 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     username = models.CharField(max_length=100)
     description = models.TextField(max_length=5000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
